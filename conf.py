6f8c7449f35d0cca1aede7b36d7c3d7679670511
Jakob Blomer <jblomer@cern.ch>
2019-03-20 11:42:32 +0100

    minor configuration tweaks

--- conf.py
+++ conf.py
@@ -160,7 +160,7 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = '%b %d, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
@@ -220,7 +220,7 @@ htmlhelp_basename = 'CernVM-FSdoc'
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
 #'pointsize': '10pt',
