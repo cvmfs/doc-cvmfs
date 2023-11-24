78db71e83d6c48904ab0872fa820dfeb85981fc8
Valentin Volkl <valentin.volkl@cern.ch>
2023-11-24 17:58:36 +0100

    use tabsets in quickstart instructions (#222)
    * use tabsets in quickstart instructions
    
    * use tabsets in quickstart instructions

--- conf.py
+++ conf.py
@@ -34,6 +34,7 @@ extensions = [
     'sphinxcontrib.mermaid',
     'sphinx_rtd_theme',
     'sphinxcontrib.cairosvgconverter',
+    'sphinx_design',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
